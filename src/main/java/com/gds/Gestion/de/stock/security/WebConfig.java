51301c58534bc167bd98eecc5488199792d68082
sogoredev <sogoredev@gmail.com>
2024-12-09 10:29:23 +0000

    set up proxy

---
+++ src/main/java/com/gds/Gestion/de/stock/security/WebConfig.java
@@ -0,0 +1,21 @@
+package com.gds.Gestion.de.stock.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+//        test
+        registry.addMapping("/**")
+                .allowedOrigins("http://api.stock.g2sservice.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
