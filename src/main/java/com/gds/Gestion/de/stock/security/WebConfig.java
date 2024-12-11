4ad3577c40c0fb29e52744bc5090287c9be2e6da
sogoredev <sogoredev@gmail.com>
2024-12-11 15:17:34 +0000

    Résolution des erreur

--- src/main/java/com/gds/Gestion/de/stock/security/WebConfig.java
+++ src/main/java/com/gds/Gestion/de/stock/security/WebConfig.java
@@ -17,5 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+//        cjnd
     }
 }
