315dd0cf97524cd86ff1073d8b5841b2af6d41d9
sogoredev <sogoredev@gmail.com>
2025-04-04 11:47:45 +0000

    test

--- src/main/java/com/gds/Gestion/de/stock/services/VenteImpl.java
+++ src/main/java/com/gds/Gestion/de/stock/services/VenteImpl.java
@@ -51,7 +51,7 @@ public class VenteImpl implements InterfaceVente {
 
     private ProduitMapper produitMapper;
 
-    //    ajouter une ventenksjabjnkcsnnkw mdvc ,sd
+    //    ajouter une ventenksjabjnkcsnnkw mdvc ,sd mscmscmsd
     @Override
     public void effectuerVente(VenteInput venteInput) throws Exception {
 
